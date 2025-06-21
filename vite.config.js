746d0bce91e03bc399af68fa8a883ea25225604f
Jamie Ly <jamie.ly@gmail.com>
2025-06-21 15:24:35 -0400

    Add code coverage command

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: 'v8', // or 'c8' for C8 provider
+      reportsDirectory: './coverage',
+      reporter: ['text', 'lcov', 'json-summary'], // formats
+      all: true, // include files not tested
+      exclude: [...configDefaults.exclude, 'src/generated/**'],
+    },
     environment: "jsdom",
     globals: true,
   },
