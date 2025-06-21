969fb994f15c16549ceda61d4e9b1a3e9dfd49f7
Jamie Ly <jamie.ly@gmail.com>
2025-06-21 11:22:08 -0400

    Separated sound module

--- src/main.js
+++ src/main.js
@@ -25,6 +25,12 @@ import {
   updateSkyline,
 } from './worldgen.js';
 
+import {
+  initAudio,
+  playBounceSound,
+  playGameOverSound,
+} from './sound.js'
+
 (function(root, el){
   // Input throttling
   var jumpRequested = false;             // Flag to throttle jump input to animation frames
@@ -71,56 +77,6 @@ import {
     };
   }
 
-  // Audio context for sound effects
-  var audioContext = null;
-  
-  function initAudio() {
-    try {
-      audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    } catch (e) {
-      console.log('Web Audio API not supported');
-    }
-  }
-  
-  function playBounceSound() {
-    if (!audioContext) return;
-    
-    var oscillator = audioContext.createOscillator();
-    var gainNode = audioContext.createGain();
-    
-    oscillator.connect(gainNode);
-    gainNode.connect(audioContext.destination);
-    
-    // Create a quick bounce sound
-    oscillator.frequency.setValueAtTime(220, audioContext.currentTime);
-    oscillator.frequency.exponentialRampToValueAtTime(440, audioContext.currentTime + 0.1);
-    
-    gainNode.gain.setValueAtTime(0.3, audioContext.currentTime);
-    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.2);
-    
-    oscillator.start(audioContext.currentTime);
-    oscillator.stop(audioContext.currentTime + 0.2);
-  }
-  
-  function playGameOverSound() {
-    if (!audioContext) return;
-    
-    var oscillator = audioContext.createOscillator();
-    var gainNode = audioContext.createGain();
-    
-    oscillator.connect(gainNode);
-    gainNode.connect(audioContext.destination);
-    
-    // Create a descending game over sound
-    oscillator.frequency.setValueAtTime(330, audioContext.currentTime);
-    oscillator.frequency.exponentialRampToValueAtTime(165, audioContext.currentTime + 0.5);
-    
-    gainNode.gain.setValueAtTime(0.4, audioContext.currentTime);
-    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.8);
-    
-    oscillator.start(audioContext.currentTime);
-    oscillator.stop(audioContext.currentTime + 0.8);
-  }
   
   // High Score Management
   function getHighScores() {
